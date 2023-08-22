1d9d582bd1bae864d432c5edbc36d88d17e9c2ec
martins-o <jojololamartins686@gmail.com>
2023-08-22 14:35:33 +0100

    Types basics

---
+++ typesBasics.js
@@ -0,0 +1,7 @@
+var character = 'Martins';
+var age = 30;
+var isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter = Math.PI;
+};
+console.log(circ(23));
---
+++ typesBasics.ts
@@ -0,0 +1,10 @@
+let character = 'Martins';
+let age = 30;
+let isBlackBelt = false;
+
+
+const circ = (diameter) => {
+    return diameter = Math.PI
+}
+
+console.log(circ(23));
