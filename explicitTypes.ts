418c94df64b61c5ed0b042471ded0423ce35ab82
martins-o <jojololamartins686@gmail.com>
2023-08-22 14:59:44 +0100

    explicit types

---
+++ explicitTypes.ts
@@ -0,0 +1,22 @@
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+let marts: string[];
+marts = ['marts', 'semicolon']
+
+// union types
+let mixed: (string|number|boolean)[] = [];
+mixed.push('marts')
+mixed.push(23)
+mixed.push(true)
+
+let uid: string|number;
+uid = 'marts';
+uid = 54;
+
+// objects
+
+let martsObject: object;
+martsObject = {name: 'marts', age : 3600}
+
